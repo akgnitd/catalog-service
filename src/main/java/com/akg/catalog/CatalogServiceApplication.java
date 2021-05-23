5afb5652b95c712ce5bce1be2657d4f1325d1bf5
akg <akgnit7@gmail.com>
2021-05-23 12:52:23 +0530

    Swagger Added

--- src/main/java/com/akg/catalog/CatalogServiceApplication.java
+++ src/main/java/com/akg/catalog/CatalogServiceApplication.java
@@ -2,12 +2,13 @@ package com.akg.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
-
 }
